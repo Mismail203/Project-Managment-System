36a2080633c0fd453f00a2d1a9dbc0ce4ee1143d
M_ismail <110721685+Mismail203@users.noreply.github.com>
2023-06-30 21:18:35 +0500

    IMAGES

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
-if(empty($_SESSION['email']))
+if(empty($_SESSION['username']))
 {
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <link rel="stylesheet" type="text/css" href="../css.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style>
 	body
@@ -25,46 +24,42 @@ if(empty($_SESSION['email']))
 <table width="100%"  cellspacing="00" cellpadding="00">
   <tr bgcolor="#D2691E">
     <th width="7%" scope="col">&nbsp;</th>
-    <th width="12%" scope="col"><img src="images/logo1.png" alt="LOGO"/></th>
+    <th width="12%" scope="col"><img src="images/logo1.png" alt="LOGO"></th>
     <th width="62%" scope="col"><font size="8" color="White">Project Managenent System</font></th>
-    <th width="13%" scope="col"><font size="5" color="White">&nbsp;</font></th>
+    <th width="13%" scope="col"><font size="5" color="White">Login</font></th>
     <th width="6%" scope="col">&nbsp;</th>
   </tr>
   <tr>
     <td>&nbsp;</td>
     <td>&nbsp;</td>
-    <td><br/><br/><div style="width:50%;background-color:#FFF8DC;margin-left:24%;margin-top:100px;border:1px solid black;">
+    <td><div style="width:40%;background-color:#FFF8DC;margin-left:30%;margin-top:100px;font-family:calibri;border:1px solid black;">
     	<br><br>
-                <form name="login" action="chk.php" method="post">
-                    
-        <table width="100%"  cellspacing="02" cellpadding="05">
+        <form name="login" action="register.php" method="post">
+        <table width="100%"  cellspacing="02" cellpadding="02">
   <tr>
-      <th colspan="2" scope="col"><font size="6">LOGIN</font></th>
+    <th colspan="2" scope="col">LOGIN</th>
     </tr>
   <tr>
-      <td align="right"><font size="5">ID&nbsp;:&nbsp;</font></td>
-    <td><input style="height: 20px; font-size: 15px;" type="text" name="id"/><br/>
-    </td>
+    <td align="right">Email&nbsp;:&nbsp;</td>
+    <td><input type="email" name="name"><br/>
+    <span class="error">* <?php echo $nameErr;?></span></td>
   </tr>
   <tr>
-      <td align="right"><font size="5">Password&nbsp;:&nbsp;</font></td>
-    <td><input style="height: 20px; font-size: 15px;" type="password" name="pass" /></td>
+    <td align="right">Password&nbsp;:&nbsp;</td>
+    <td><input type="password" name="name"></td>
   </tr>
   <tr>
-      <td align="right"><font size="5">Login_As&nbsp;:&nbsp;</font></td>
+    <td align="right">Login_As&nbsp;:&nbsp;</td>
     <td>
-        <select name="role" style="width: 13em; height: 2em; font-size: 15px;">
+      <select>
         <option value="Student">Student</option>
-        <option value="Faculty">Faculty</option>
+        <option value="Supervisor">Supervisor</option>
         <option value="Admin">Admin</option>          
         </select>
       </td>
   </tr>
-            <tr>
-                <td colspan="2" align="center"><input type="submit" style="width: 4em;  height: 2em; font-size: 20px;" name="register" value="Submit" /></td>
-            </tr>
-</table> 
-
+</table>    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="submit" name="register" value="Submit" />
         <br/>
         &nbsp;
         </form>
@@ -83,14 +78,12 @@ if(empty($_SESSION['email']))
 </table>
 </body>
 </div>
-    
 </html>
-
 <?php
 }
 else
 {
-header("location:Admin.php");
+header("location:index.php");
 }
 
 ?>
